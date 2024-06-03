660d3f05a986982c0485b8ddc017df0589265827
Pranav <pranavmane295@gmail.com>
2024-06-03 21:50:47 +0530

    Created HTML, CSS, JS file

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 document.querySelectorAll('nav a').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
@@ -9,15 +8,3 @@ document.querySelectorAll('nav a').forEach(anchor => {
     });
 });
 feather.replace();
-=======
-
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-feather.replace();
->>>>>>> 9fe891fe5cdf852acfb921732da266a8a9c9613c
